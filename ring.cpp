156b0b277febefe5787ec08cd085b1f7d8b31219
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-02-12 13:34:12 +0530

    Create ring.cpp

---
+++ ring.cpp
@@ -0,0 +1,120 @@
+#include<iostream.h>
+#include<conio.h>
+
+class RING
+{
+	int RingNumber;
+	float Radius,Area;
+	void CalcArea()
+	{
+		Area=3.14*Radius*Radius;
+	}
+	public:
+	void GetArea()
+	{
+		cout<<"\n\tENTER THE VALUE OF RING NUMBER:";
+		cin>>RingNumber;
+		cout<<"\n\tENTER THE RADIUS:";
+		cin>>Radius;
+		CalcArea();
+	}
+	void ShowArea()
+	{
+		cout<<"\n\tTHE VALUE OF RING NUMBER IS:"<<RingNumber;
+		cout<<"\n\tTHE VALUE OF RADIUS IS:"<<Radius;
+		cout<<"\n\tTHE VALUE OF AREA IS:"<<Area;
+	}
+};
+void main()
+{
+	clrscr();
+	RING R1;
+	R1.GetArea();
+	R1.ShowArea();
+	getch();
+}
+
+
+
+
+
+#include<iostream.h>
+#include<conio.h>
+
+class RING
+{
+	int RingNumber;
+	float Radius,Area;
+	void CalcArea()
+	{
+		Area=3.14*Radius*Radius;
+	}
+	public:
+	void GetArea()
+	{
+		cout<<"\n\tENTER THE VALUE OF RING NUMBER:";
+		cin>>RingNumber;
+		cout<<"\n\tENTER THE RADIUS:";
+		cin>>Radius;
+		CalcArea();
+	}
+	void ShowArea()
+	{
+		cout<<"\n\tTHE VALUE OF RING NUMBER IS:"<<RingNumber;
+		cout<<"\n\tTHE VALUE OF RADIUS IS:"<<Radius;
+		cout<<"\n\tTHE VALUE OF AREA IS:"<<Area;
+	}
+};
+void main()
+{
+	clrscr();
+	RING R1;
+	R1.GetArea();
+	R1.ShowArea();
+	getch();
+}
+
+
+
+
+
+#include<iostream.h>
+#include<conio.h>
+
+class RING
+{
+	int RingNumber;
+	float Radius,Area;
+	void CalcArea()
+	{
+		Area=3.14*Radius*Radius;
+	}
+	public:
+	void GetArea()
+	{
+		cout<<"\n\tENTER THE VALUE OF RING NUMBER:";
+		cin>>RingNumber;
+		cout<<"\n\tENTER THE RADIUS:";
+		cin>>Radius;
+		CalcArea();
+	}
+	void ShowArea()
+	{
+		cout<<"\n\tTHE VALUE OF RING NUMBER IS:"<<RingNumber;
+		cout<<"\n\tTHE VALUE OF RADIUS IS:"<<Radius;
+		cout<<"\n\tTHE VALUE OF AREA IS:"<<Area;
+	}
+};
+void main()
+{
+	clrscr();
+	RING R1;
+	R1.GetArea();
+	R1.ShowArea();
+	getch();
+}
+
+
+
+
+
